daac5e33a74d9a4f9a18cb448291a17a83160f68
Sean McElroy <sean.mcelroy@sonitussystems.com>
2023-02-25 22:08:14 +0000

    added pandas

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -5,9 +6,12 @@ app = Flask(__name__)
 # define a simple endpoint that takes a GET request
 @app.route('/hello', methods=['GET'])
 def hello():
+    df = pd.read_csv('dummy.csv')
     name = request.args.get('name', 'World')
     response = {'message': f'Hello, {name}!'}
-    return jsonify(response)
+    # return jsonify(response)
+    data = df.to_dict(orient='records')
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
