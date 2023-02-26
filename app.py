ef80fb461c7a0b7e7d848fb07c383ac32ea4b949
Sean MceLroy <seanpetermcelroy@gmail.com>
2023-02-26 10:40:00 +0000

    Added coords parameter that accepts string

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # define a simple endpoint that takes a GET request
+
+
 @app.route('/hello', methods=['GET'])
 def hello():
     df = pd.read_csv('dummy.csv')
-    name = request.args.get('name', 'World')
+    name = request.args.get('coords', 'World')
     response = {'message': f'Hello, {name}!'}
-    # return jsonify(response)
-    data = df.to_dict(orient='records')
-    return jsonify(data)
+    return jsonify(response)
+    # data = df.to_dict(orient='records')
+    # return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
